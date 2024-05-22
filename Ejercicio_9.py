010f8795ccd5096c8a231e4658d4912a61330cb8
NashRick0 <ricardolunaunzueta97@gmail.com>
2024-05-22 07:36:21 -0600

    programa 9 - contraseña

---
+++ Ejercicio_9.py
@@ -0,0 +1,9 @@
+password = "POO123"
+
+while True:
+    pas = input("Ingrese la contraseña: ")
+    if pas=="POO123":
+        print("Correcto, ¡Bienvenido!")
+        break
+    else:
+        print("Incorrecta, ¡Vuelva a Intentarlo!")
