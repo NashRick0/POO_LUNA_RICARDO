15b5b721322b59522936520f6e042a5a3e9d568f
NashRick0 <ricardolunaunzueta97@gmail.com>
2024-05-27 10:43:47 -0600

    Examen Parcial 1

---
+++ Examen_P1.py
@@ -0,0 +1,9 @@
+print("Numeros Positivos, Negativos o Cero")
+print("")
+num = float(input("Ingrese su numero: "))
+if num >0:
+    print("Tu numero es POSITIVO")
+elif num <0:
+    print("Tu numero es NEGATIVO")
+elif num == 0:
+    print("Es 0")
