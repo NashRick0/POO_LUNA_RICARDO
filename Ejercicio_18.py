2999c54d84e49ce0ca990d25fb4dd8c643e27dbe
NashRick0 <ricardolunaunzueta97@gmail.com>
2024-05-30 17:42:48 -0600

    Programa 18 - Diccionario con descuento

---
+++ Ejercicio_18.py
@@ -0,0 +1,17 @@
+print("\nPODRUCTOS")
+
+diccionario = {
+    "Coca" : 20,
+    "Jabon" : 40,
+    "Arroz" : 30
+}
+des = .30
+
+for prod, precio in diccionario.items():
+    print(f"{prod}: {precio}")
+
+print("\nDescuentos")
+
+for prod, precio in diccionario.items():
+    diccionario[prod] = diccionario[prod]*(1 - des)
+    print(f"{prod}: {diccionario[prod]}")
