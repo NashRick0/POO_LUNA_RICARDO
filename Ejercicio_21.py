c023c28f757f7de57cc0869aa9de6b1a6b4e6fe8
NashRick0 <ricardolunaunzueta97@gmail.com>
2024-06-03 11:48:58 -0600

    Programa 21 - tupla desempaquetada

---
+++ Ejercicio_21.py
@@ -0,0 +1,9 @@
+datos = ('Rick', '19', 'Enero', 'Hombre', 'Estudiando')
+
+(var1, var2, var3, var4, var5) = datos
+
+print(f"Nombre: {var1}")
+print(f"Edad: {var2}")
+print(f"Mes Nacimiento: {var3}")
+print(f"Genero: {var4}")
+print(f"Situacion: {var5}")
