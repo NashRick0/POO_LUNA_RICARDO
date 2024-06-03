8009e5fd787e0481e80d9a0e1599f1bae2fc41b3
NashRick0 <ricardolunaunzueta97@gmail.com>
2024-06-03 11:47:47 -0600

    Programa 19 - conjuntos practica

---
+++ Ejercicio_19.py
@@ -0,0 +1,8 @@
+conjunto1 = {"Pera", "Manzana", "Sandia"}
+print(conjunto1)
+conjunto2 = {"Kiwi", "Platano", "Melon"}
+print(conjunto2)
+
+union = conjunto1 | conjunto2
+conjunto1.union(conjunto2)
+print(f"Nuevo conjunto: {union}")
