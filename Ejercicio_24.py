fb2fa6a0e34973ab35feb3cb6832ba09fe2a86ef
NashRick0 <ricardolunaunzueta97@gmail.com>
2024-06-25 07:22:02 -0600

    Programa 24 - Asociacion

--- Ejercicio_24.py
+++ Ejercicio_24.py
@@ -1,3 +1,4 @@
+#CLASE PAJARO
 class Pajaro:
     def __init__(self, tipo, habitad):
         self.tipo = tipo
@@ -12,6 +13,7 @@ class Pajaro:
     def cantar(self):
         print("Cantando")
 
+#CLASE ZOO
 class Zoo:
     def __init__(self, tipo):
         self.tipo = tipo 
@@ -25,13 +27,38 @@ class Zoo:
         print(self.tipo, "tiene estos tipos de pajaros: ")
         for pajaro in self.pajaros:
             print(pajaro.tipo)
+
+#CLASE CUIDADOR
+class Cuidador:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.pajaros = []
+
+    def asignar_pajaro(self, pajaro):
+        self.pajaros.append(pajaro)
+        print(pajaro.tipo, "asignado a", self.nombre)
+
+    def lista_pajaros_cuidados(self):
+        print(self.nombre, "cuida de estos pájaros: ")
+        for pajaro in self.pajaros:
+            print(pajaro.tipo)
+
     
+#OBJETOS PAJAROS
+
 pajaro1 = Pajaro("Canario", "Bosques")
 pajaro2 = Pajaro("Avestruz", "Sabanas")
 
-zoo = Zoo("El Oeste")
+#METODOS ZOO
 
+zoo = Zoo("El Oeste")
 zoo.añadir_pajaro(pajaro1)
 zoo.añadir_pajaro(pajaro2)
-
 zoo.lista_pajaros()
+
+#CLASE CUIDADOR
+
+cuidador1 = Cuidador("Rick")
+cuidador1.asignar_pajaro(pajaro1)
+cuidador1.asignar_pajaro(pajaro2)
+cuidador1.lista_pajaros_cuidados()
