6c526724d3dea23d035cb1f8249e67eb57af318d
NashRick0 <ricardolunaunzueta97@gmail.com>
2024-07-01 19:55:28 -0600

    Programa 24 - De 24 a 28

--- Ejercicio_24.py
+++ Ejercicio_24.py
@@ -1,4 +1,4 @@
-#CLASE PAJARO
+#CLASE PAJARO (Principal)
 class Pajaro:
     def __init__(self, tipo, habitad):
         self.tipo = tipo
@@ -13,11 +13,15 @@ class Pajaro:
     def cantar(self):
         print("Cantando")
 
-#CLASE ZOO
+    def construir(self, nido):
+        print(self.tipo, "esta construyendo un nido de", nido.material)
+
+#CLASE ZOO (Asociacion)
 class Zoo:
     def __init__(self, tipo):
         self.tipo = tipo 
         self.pajaros = []
+        self.habitads = []
     
     def añadir_pajaro(self, pajaro):
         self.pajaros.append(pajaro)
@@ -28,7 +32,16 @@ class Zoo:
         for pajaro in self.pajaros:
             print(pajaro.tipo)
 
-#CLASE CUIDADOR
+    def añadir_habitad(self, habitad):
+        self.habitads.append(habitad)
+        print("Habitad", habitad.tipo, "añadido al zoo", self.tipo)
+
+    def lista_habitads(self):
+        print(self.tipo, "tiene estos habitads: ")
+        for habitad in self.habitads:
+            print(habitad.tipo)
+
+#CLASE CUIDADOR (Agregacion)
 class Cuidador:
     def __init__(self, nombre):
         self.nombre = nombre
@@ -43,22 +56,72 @@ class Cuidador:
         for pajaro in self.pajaros:
             print(pajaro.tipo)
 
+#CLASE HABITAD (Composicion)
+class Habitad:
+    def __init__(self, tipo):
+        self.tipo = tipo
+        self.pajaros = []
+
+    def añadir_pajaro(self, pajaro):
+        self.pajaros.append(pajaro)
+        print(pajaro.tipo, "añadido al habitad", self.tipo)
     
-#OBJETOS PAJAROS
+    def lista_pajaros(self):
+        print(self.tipo, "tiene estos tipos de pajaros: ")
+        for pajaro in self.pajaros:
+            print(pajaro.tipo)
 
-pajaro1 = Pajaro("Canario", "Bosques")
-pajaro2 = Pajaro("Avestruz", "Sabanas")
+#CLASE PAJARO MENSAJERO (Herencia)
 
-#METODOS ZOO
+class PajaroMensajero(Pajaro):
+    def __init__(self, tipo, habitad, tipo_mensaje):
+        super().__init__(tipo, habitad)
+        self.tipo_mensaje = tipo_mensaje
 
-zoo = Zoo("El Oeste")
+    def trabajar(self):
+        print(self.tipo, "esta trabajando como pajaro de mensajeria de",
+              self.tipo_mensaje)
+
+#CLASE NIDO (Dependencia)
+
+class Nido():
+    def __init__(self, material):
+        self.material = material
+
+    def describir(self):
+        print("El nido esta hecho de", self.material)
+
+    
+#METODO PAJARO
+
+pajaro1 = Pajaro("Canario", "Bosques")  #Objeto
+pajaro2 = Pajaro("Avestruz", "Sabanas") #Objeto
+habitad = Habitad("Bosque")
+habitad.añadir_pajaro(pajaro1)
+
+#METODO ZOO
+
+zoo = Zoo("El Oeste") #Objeto
 zoo.añadir_pajaro(pajaro1)
 zoo.añadir_pajaro(pajaro2)
 zoo.lista_pajaros()
 
-#CLASE CUIDADOR
+zoo.añadir_habitad(habitad)
+zoo.lista_habitads()
 
-cuidador1 = Cuidador("Rick")
+#METODO CUIDADOR
+
+cuidador1 = Cuidador("Rick") #Objeto
 cuidador1.asignar_pajaro(pajaro1)
 cuidador1.asignar_pajaro(pajaro2)
 cuidador1.lista_pajaros_cuidados()
+
+#METODO PAJARO_MENSAJERO
+
+pajaro_mensaje = PajaroMensajero("Canario", "Bosque", "Cartas") #Objeto
+pajaro_mensaje.trabajar()
+
+#METODO NIDO
+
+nido1 = Nido("Paja") #Objeto
+pajaro1.construir(nido1)
